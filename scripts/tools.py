76debf7fe018d78a0ab98e2078d33fe4d5ac866d
parichehrvn <p.vahidinia@gmail.com>
2025-09-19 10:27:52 +0330

    add tools search_flights

--- scripts/tools.py
+++ scripts/tools.py
@@ -18,6 +18,7 @@ def lookup_policy(query: str) -> str:
     docs = retriever.invoke(query)
     return "\n\n".join(doc["page_content"] for doc in docs)
 
+
 @tool
 def fetch_user_flight_information(config: RunnableConfig, db="../data/travel.sqlite") -> list[dict]:
     """Fetch all tickets for the user along with corresponding flight information and seat assignments.
@@ -60,4 +61,51 @@ def fetch_user_flight_information(config: RunnableConfig, db="../data/travel.sql
 
     except KeyError as e:
         print(e)
+
+
+@tool
+def search_flights(
+    departure_airport: Optional[str] = None,
+    arrival_airport: Optional[str] = None,
+    start_time: Optional[date | datetime] = None,
+    end_time: Optional[date | datetime] = None,
+    limit: int = 20,
+    db="../data/travel.sqlite"
+) -> list[dict]:
+    """Search for flights based on departure airport, arrival airport, and departure time range."""
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM flights WHERE 1 = 1"
+    params = []
+
+    if departure_airport:
+        query += "AND departure_airport = ?"
+        params.append(departure_airport)
+
+    if arrival_airport:
+        query += " AND arrival_airport = ?"
+        params.append(arrival_airport)
+
+    if start_time:
+        query += " AND scheduled_departure >= ?"
+        params.append(start_time)
+
+    if end_time:
+        query += " AND scheduled_departure <= ?"
+        params.append(end_time)
+
+    query += "LIMIT ?"
+    params.append(limit)
+
+    cursor.execute(query, params)
+    rows = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    results = [dict(zip(column_names, row)) for row in rows]
+
+    cursor.close()
+    conn.close()
+
+    return results
+
 
