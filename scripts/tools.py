5204f2894f0fce5c92bd023ae4d972f4402aaca3
parichehrvn <p.vahidinia@gmail.com>
2025-09-19 13:47:00 +0330

    add tool for cancle ticket

--- scripts/tools.py
+++ scripts/tools.py
@@ -179,4 +179,46 @@ def update_ticket_to_new_flight(
         print(e)
 
 
+@tool
+def cancel_ticket(ticket_no : str, *, config : RunnableConfig, db="../data/travel.sqlite") -> str:
+    """Cancel the user's ticket and remove it from the database."""
+    try:
+        configuration = config["configurable"]
+        passenger_id = configuration["passenger_id"]
 
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT flight_id FROM ticket_flights WHERE ticket_no = ?",
+            (ticket_no,)
+        )
+        existing_ticket = cursor.fetchone()
+        if not existing_ticket:
+            cursor.close()
+            conn.close()
+            return "No existing ticket found for the given ticket number."
+
+        # Check the signed-in user actually has this ticket
+        cursor.execute(
+            "SELECT ticket_no FROM tickets WHERE ticket_no = ? AND passenger_id = ?",
+            (ticket_no, passenger_id)
+        )
+        current_ticket = cursor.fetchone()
+        if not current_ticket:
+            cursor.close()
+            conn.close()
+            return f"Current signed-in passenger with ID {passenger_id} not the owner of ticket {ticket_no}"
+
+        cursor.execute(
+            "DELETE FROM ticket_flights WHERE ticket_no = ?",
+            (ticket_no,)
+        )
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+        return "Ticket successfully cancelled."
+
+    except KeyError as e:
+        print(e)
